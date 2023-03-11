acd9c8373a73aa7fb35aceb0d819d986404bc238
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-11 22:02:17 +0200

    v@10

--- index.js
+++ index.js
@@ -11,78 +11,78 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// mongoose
-//   .connect(config.DBURL)
-//   .then((res) => {
-//     console.log("connected to database");
-//   })
-//   .catch((error) => {
-//     console.log("connection to db error-", error);
-//   });
+mongoose
+  .connect(config.DBURL)
+  .then((res) => {
+    console.log("connected to database");
+  })
+  .catch((error) => {
+    console.log("connection to db error-", error);
+  });
 
-// app.post("/users/userCreate", (req, res) => {
-//   Users.create({
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
-//   console.log("new user created", {
-//     name: req.body.name,
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
-// });
+app.post("/users/userCreate", (req, res) => {
+  Users.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+  console.log("new user created", {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+});
 
-// app.post("/users", (req, res) => {
-//   Users.find({
-//     name: req.body.name,
-//     email: req.body.email,
-//   })
-//     .then((users) => {
-//       //users is an array
-//       const user = users.find((user) => user.name === req.body.name);
+app.post("/users", (req, res) => {
+  Users.find({
+    name: req.body.name,
+    email: req.body.email,
+  })
+    .then((users) => {
+      //users is an array
+      const user = users.find((user) => user.name === req.body.name);
 
-//       const RequestPassword = req.body.password;
-//       // console.log("this user", user, RequestPassword);
-//       if (RequestPassword === user.password) {
-//         const response = {
-//           status: "authorized",
-//           user: {
-//             name: user.name,
-//             email: user.email,
-//           },
-//         };
-//         res.send(response);
-//       } else if (RequestPassword != user.password) {
-//         const response = { status: "invalid passoword" };
-//         res.send(response);
-//       }
-//     })
-//     .catch((error) => res.send(error));
-// });
+      const RequestPassword = req.body.password;
+      // console.log("this user", user, RequestPassword);
+      if (RequestPassword === user.password) {
+        const response = {
+          status: "authorized",
+          user: {
+            name: user.name,
+            email: user.email,
+          },
+        };
+        res.send(response);
+      } else if (RequestPassword != user.password) {
+        const response = { status: "invalid passoword" };
+        res.send(response);
+      }
+    })
+    .catch((error) => res.send(error));
+});
 
-// app.get("/users", (req, res) => {
-//   Users.find().then((users) => {
-//     res.send(users);
-//   });
-// });
+app.get("/users", (req, res) => {
+  Users.find().then((users) => {
+    res.send(users);
+  });
+});
 
-// app.get("/comments", (req, res) => {
-//   Comments.find().then((comments) => {
-//     res.send(comments);
-//   });
-// });
+app.get("/comments", (req, res) => {
+  Comments.find().then((comments) => {
+    res.send(comments);
+  });
+});
 
-// app.post("/comments/create", (req, res) => {
-//   Comments.create({
-//     author: req.body.author.name,
-//     authoremail: req.body.author.email,
-//     text: req.body.text,
-//   }).then(res.send(
-//    {response: "your comment went to db"}
-//   ));
-//   // console.log("new comment");
-// });
+app.post("/comments/create", (req, res) => {
+  Comments.create({
+    author: req.body.author.name,
+    authoremail: req.body.author.email,
+    text: req.body.text,
+  }).then(res.send(
+   {response: "your comment went to db"}
+  ));
+  // console.log("new comment");
+});
 
 // app.get("/comments", (req, res) => {
 //Comments.create({name email text}) for create a new comments in db
@@ -115,3 +115,7 @@ app.use(cors());
 app.listen(config.PORT, () => {
   console.log(`Server has been startted on ${config.PORT}...`);
 });
+
+export default {
+  app
+}
