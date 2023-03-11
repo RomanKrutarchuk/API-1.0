fe3d53eb7b87975394c2bd80aaf131d055ce93bb
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-11 19:28:45 +0200

    readme

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export default {
   PORT: "3000",
   DBURL: "mongodb+srv://ApiAdmin01:1q2w3e@clusterpfc.huvtbcz.mongodb.net/Users?retryWrites=true&w=majority"
 }
-//for publish code on repository 1.git remote add origin https:// 2.
+//for publish code on repository 1.git remote add origin https:// 1.5git remote -v 2.git push -u origin master
