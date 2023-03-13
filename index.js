030f557907c9e4e9a0b7aa9c680719ae26cdec38
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-13 19:33:33 +0200

    origin

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: api_url,
+    origin: "https://vercel-pfc-repository-web.vercel.app",
     methods: ["GET", "POST"],
   },
 });
