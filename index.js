60b0335b551667325f4093c3e496297afad63c36
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-13 20:33:29 +0200

    cors origin

--- index.js
+++ index.js
@@ -14,19 +14,7 @@ const Users = modules.Users;
 
 app.use(express.json());
 app.use(cors());
-const io = new Server(server, {
-  // handlePreflightRequest: (req, res) => {
-  //   const headers = {
-  //     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  //     "Access-Control-Allow-Origin":
-  //       "https://vercel-pfc-repository-web.vercel.app/comments", //or the specific origin you want to give access to,
-  //     "Access-Control-Allow-Credentials": true,
-  //   };
-  //   res.writeHead(200, headers);
-  //   res.end();
-  // },
-});
-io.set("origins", "https://vercel-pfc-repository-web.vercel.app/comments");
+const io = new Server(server, { cors: { origin: "*" } });
 
 app.get("/", (req, res) => {
   res.send("hello vercel");
