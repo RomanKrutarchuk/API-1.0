b69f347dab7346e1ee6620cf63489bcbcd37e8fc
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-15 22:16:04 +0200

    config

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
-//for procces.env run bash: export NODE_ENV="development"
+//for procces.env run bash: 
+// export NODE_ENV="development"
 let APP_ORIGIN = "https://vercel-pfc-repository-web.vercel.app";
 let START_ON = "https://vercel-pfc-repository-api.vercel.app";
+const PORT = 3000
 
 if (process.env.NODE_ENV === "development") {
-  APP_ORIGIN = "http://localhost:8080";
-  START_ON = "http://localhost:3000";
+  APP_ORIGIN =`http://localhost:${PORT}`;
+  START_ON = `http://localhost:${PORT}`;
 }
 
 export default {
-  PORT: "3000",
+  PORT,
   DBURL:
     "mongodb+srv://ApiAdmin01:1q2w3e@clusterpfc.huvtbcz.mongodb.net/Users?retryWrites=true&w=majority",
   APP_ORIGIN,
