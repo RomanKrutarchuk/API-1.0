c4a4e79d57c1a1d21416897b3d65e5f2cdb78ee6
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-03-16 21:45:43 +0200

    headers

--- index.js
+++ index.js
@@ -12,12 +12,18 @@ const server = new http.createServer(app);
 const Comments = modules.Comments;
 const Users = modules.Users;
 app.use(express.json());
+app.use((req, res, next) => {
+  res.append('Access-Control-Allow-Origin', ['*']);
+  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
-app.use(
-  cors({
-    origin: [config.APP_ORIGIN, "*"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: [config.APP_ORIGIN, "*"],
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.send(`ORIGIN:${config.APP_ORIGIN}, START_ON:${config.START_ON}`);
