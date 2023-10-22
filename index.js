0a66d89256be5fd935e5c83b8e1faad7e6d541ba
Roman Krutarchuk <makarenaclubg@gmail.com>
2023-10-22 21:22:42 +0300

    io transport

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const io = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST", "PUT"],
-    transports: ["websocket", "webtransport"],
+    transports: ["websocket", "polling"],
   },
   pingTimeout: 50000,
   handlePreflightRequest: (req, res) => {
