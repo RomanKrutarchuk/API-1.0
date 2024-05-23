0223cd4c6a3a169634bc0fca6da335001c8a9750
Roman <makarenaclubg@mail.com>
2024-05-23 22:40:28 +0200

    tic tac

--- middleware.js
+++ middleware.js
@@ -52,55 +52,81 @@ export default function (app) {
     const body = JSON.parse(req.body);
     // console.log(body);
     if (body.token) {
-      // console.log("token");
       const token = body.token;
       const googleData = await verify(token).catch(console.error);
-      const user = await findUser(googleData)
+      const user = await findUser({ googleData })
+      console.log("/auth.user.googleData");
       res.setHeader("Content-Type", "application/json");
       res.send(JSON.stringify(user))
     }
     if (body.id) {
-      // console.log("id");
-      const user = await findUser(false, body.id)
+      const user = await findUser({ id: body.id })
+      console.log("/auth.user.id");
+      res.setHeader("Content-Type", "application/json");
+      res.send(JSON.stringify(user))
+    }
+    if (body.authData) {
+      const user = await findUser({ authData: body.authData })
+      console.log("/auth.user.authData");
       res.setHeader("Content-Type", "application/json");
       res.send(JSON.stringify(user))
     }
   });
   app.post("/getUserData", async (req, res) => {
     const body = JSON.parse(req.body);
-    console.log(body.id);
-    const user = await findUser(false, body.id)
+    // console.log(body.id);
+    const user = await findUser({ id: body.id })
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(user))
   })
 }
-async function findUser(googleData, id) {
-  if (!googleData && id != null) {
-    return await Users.find({ id }).then((users) => {
+async function findUser(value) {
+  if (value.id) {
+    return await Users.find({ id: value.id }).then((users) => {
       // console.log(users);
       const user = users[0]
       // console.log({ findUserByID: user });
       return user
     })
   }
-  return await Users.find({
-    name: googleData.name,
-    email: googleData.email,
-  }).then((users) => {
-    if (users.length === 0) {
-      const user = createUser(googleData);
+  if (value.googleData) {
+    console.log("auth.value.googleData");
+    return await Users.find({
+      name: value.googleData.name,
+      email: value.googleData.email,
+    }).then((users) => {
+      if (users.length === 0) {
+        const user = createUser(googleData);
+        return user;
+      }
+      const user = users.find((user) => user.email === user.email);
+      // console.log({ findUser: user });
       return user;
-    }
-    const user = users.find((user) => user.email === user.email);
-    // console.log({ findUser: user });
-    return user;
-  });
+    });
+  }
+  if (value.authData) {
+    console.log("auth.value.authData");
+
+    return await Users.find({ name: value.authData.name, email: value.authData.email }).then((users) => {
+      // console.log(users);
+      if (users.length === 0) {
+        const user = createUser(value.authData);
+        return user;
+      }
+      const user = users[0]
+      console.log({ findByAuthData: user });
+      return user
+    })
+  }
+
 }
-async function createUser(googleData) {
+async function createUser(data) {
+  const payload = data
+  if (!data.picture) payload.picture = "default"
   return await Users.create({
-    email: googleData.email,
-    name: googleData.name,
-    picture: googleData.picture,
+    email: payload.email,
+    name: payload.name,
+    picture: payload.picture,
   }).then((user) => {
     console.log({ createUser: user });
     return user;
--- socket.js
+++ socket.js
@@ -1,8 +1,31 @@
 
 export default function (io, connections) {
   const room = [{ id: null }, { id: null }]
+  let cells = [
+    { id: null, index: 1 },
+    { id: null, index: 2 },
+    { id: null, index: 3 },
+    { id: null, index: 4 },
+    { id: null, index: 5 },
+    { id: null, index: 6 },
+    { id: null, index: 7 },
+    { id: null, index: 8 },
+    { id: null, index: 9 },
+  ]
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  let currentWalker = 0
+
   io.on("connection", (socket) => {
-    console.log("socket connect");
+    // console.log("socket connect");
     socket.handshake.headers.origin = "*";
     let id = null
     socket.on("clientSendUserId", (data) => {
@@ -10,22 +33,25 @@ export default function (io, connections) {
       if (id) {
         connections.push(id);
         io.emit("serverEmitAllUsers", JSON.stringify(connections))
-        io.emit("Room_update", JSON.stringify(room))
-        console.log("connection-connections: ", connections);
+        emitUpdate()
+        // console.log("connection-connections: ", connections);
       }
     })
+    //
     socket.on("disconnect", () => {
       const plase = room.find((plase) => plase.id === id)
       if (plase) plase.id = null
 
       connections = connections.filter((conId) => conId !== id);
-      console.log("disconnect-connections", connections);
-      io.emit("Room_update", JSON.stringify(room))
+      // console.log("disconnect-connections", connections);
+      emitUpdate()
       io.emit("serverEmitAllUsers", JSON.stringify(connections));
+
     });
-    socket.on("Room_enter", (data) => {
+    //
+    socket.on("room.enter", (data) => {
       const { plaseIndex, id } = JSON.parse(data)
-      console.log("Room-enter", { plaseIndex, id });
+      // console.log("Room-enter", { plaseIndex, id });
       if (
         room.find((plase) => plase.id === id)
       ) {
@@ -36,14 +62,89 @@ export default function (io, connections) {
       } else {
         return console.log("plase-reserved");
       }
-      io.emit("Room_update", JSON.stringify(room))
+
+      // checkFullRoom(place)
+      emitUpdate()
     })
-    socket.on("Room_exit", (id) => {
-      console.log("Room-exit", id);
+    //
+    socket.on("room.exit", (id) => {
+      // console.log("Room-exit", id);
       const plase = room.find((plase) => plase.id === id)
       if (plase) plase.id = null
-      io.emit("Room_update", JSON.stringify(room))
+      emitUpdate()
     })
 
+    function checkFullRoom() {
+      if (room.find(place => place.id === null)) {
+        console.log("room.enter.twoPlayers:", false)
+        io.emit("room.isReady", false)
+      } else {
+        console.log("room.enter.twoPlayers:", true)
+        io.emit("room.isReady", true)
+        clearGame()
+        startGame()
+      }
+    }
+    function emitUpdate() {
+      // console.log("emitUpdate");
+      checkFullRoom()
+      io.emit("room.update", JSON.stringify(room))
+    }
+    function startGame() {
+      console.log("startGame");
+      io.emit("room.updateGame", JSON.stringify({ cells, currentWalker }))
+    }
+    function clearGame() {
+      console.log("clearGame");
+      currentWalker = 0
+      cells = [
+        { id: null, index: 0 },
+        { id: null, index: 1 },
+        { id: null, index: 2 },
+        { id: null, index: 3 },
+        { id: null, index: 4 },
+        { id: null, index: 5 },
+        { id: null, index: 6 },
+        { id: null, index: 7 },
+        { id: null, index: 8 },
+      ]
+    }
+    //
+    socket.on("selectCell", (data) => {
+      const select = JSON.parse(data)
+      const index = select.index;
+      const id = select.id
+      // console.log({ index, id });
+      cells.find(cell => cell.index === index).id = id
+      if (playerWin(id)) io.emit("room.playerWin", id)
+      if (currentWalker == 0) {
+        currentWalker = 1
+      } else {
+        currentWalker = 0
+      }
+      // console.log({ cells, currentWalker });
+      io.emit("room.updateGame", JSON.stringify({ cells, currentWalker }))
+    })
+    function playerWin(id) {
+      if (!cells.find(cell => !cell.id)) {
+        console.log("full");
+        setTimeout(() => {
+          clearGame()
+          startGame()
+        }, 2000)
+      }
+      for (let combination of winningCombinations) {
+        const [a, b, c] = combination;
+        if (cells[a].id === id && cells[b].id === id && cells[c].id === id) {
+          setTimeout(() => {
+            clearGame()
+            startGame()
+          }, 2000)
+          return true
+        }
+      }
+      return false;
+    }
   });
 }
+
